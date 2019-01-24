95dec0d351f466366e0ee4cb60c7df6ae1f38c20
JorgeBoado <jorge.boadogo@elorrieta-errekamari.com>
2019-01-24 14:20:50 +0100

    Initial commit

---
+++ app/src/main/java/easylodging/com/ethazi_easylodging/SplashScreen.java
@@ -0,0 +1,34 @@
+package easylodging.com.ethazi_easylodging;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreen.this)
+                .withFullScreen()
+                .withTargetActivity(Login.class)
+                .withBackgroundColor(Color.parseColor("#282C37"))
+                .withSplashTimeOut(3000)
+                .withLogo(R.drawable.ic_main_logo)
+                .withFooterText("Aritz, Jon, Kevin, Jorge");
+
+        //Text Color
+        config.getFooterTextView().setTextColor(Color.parseColor("#1C6434"));
+
+
+        //Set to view
+        View view = config.create();
+
+        //Set view to content view
+        setContentView(view);
+    }
+}
