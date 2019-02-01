072c27d1e833d6128813f4346b09064976231517
JorgeBoado <jorge.boadogo@elorrieta-errekamari.com>
2019-02-01 14:23:26 +0100

    Commit 1.6

--- app/src/main/java/easylodging/com/ethazi_easylodging/Log/Activity/SignInManager.java
+++ app/src/main/java/easylodging/com/ethazi_easylodging/Log/Activity/SignInManager.java
@@ -3,18 +3,19 @@ package easylodging.com.ethazi_easylodging.Log.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import easylodging.com.ethazi_easylodging.Log.Fragment.SignIn;
 import easylodging.com.ethazi_easylodging.Main.NavigationDrawer;
 import easylodging.com.ethazi_easylodging.R;
 
-public class LogInManager extends AppCompatActivity {
-
+public class SignInManager extends AppCompatActivity {
     private static final String TAG_LOGIN="Logueado";
     private static final String TAG_SIGNIN = "Lanzando SignIn";
 
@@ -24,21 +25,29 @@ public class LogInManager extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signin);
+        Button btn_SignIn= findViewById(R.id.signin_button);
+        CheckBox cbx_TC= findViewById(R.id.signin_chechbox);
 
-        Button btn_LogIn= findViewById(R.id.login_btn);
-        TextView txv_Signin= findViewById(R.id.login_signup);
-
-        btn_LogIn.setOnClickListener(new View.OnClickListener() {
+        btn_SignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                userValidation();
+                userSignIn();
             }
         });
-        txv_Signin.setOnClickListener(new View.OnClickListener() {
+        cbx_TC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                launchSignIn();
+                new AlertDialog().Builder(this)
+                        .setMessage(getString(R.string.dialog_logoff))
+                        .setCancelable(false)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                transactBack();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.cancel, null)
+                        .show();
             }
         });
 
@@ -50,7 +59,7 @@ public class LogInManager extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void userValidation() {
+    private void userSignIn() {
         Log.d(TAG_LOGIN, "Se ha clicado");
         Intent intent = new Intent(this, NavigationDrawer.class);
         startActivity(intent);
--- app/src/main/java/easylodging/com/ethazi_easylodging/Main/NavigationDrawer.java
+++ app/src/main/java/easylodging/com/ethazi_easylodging/Main/NavigationDrawer.java
@@ -32,7 +32,6 @@ public class NavigationDrawer extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
