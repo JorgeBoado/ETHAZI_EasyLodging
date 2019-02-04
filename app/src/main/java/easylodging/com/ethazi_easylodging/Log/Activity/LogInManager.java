e3da469c8972289bc0cfa601ba61a5ca8b3fc86b
JorgeBoado <jorge.boadogo@elorrieta-errekamari.com>
2019-02-04 14:16:52 +0100

    Correción de proyecto

--- app/src/main/java/easylodging/com/ethazi_easylodging/Log/Activity/LogInManager.java
+++ app/src/main/java/easylodging/com/ethazi_easylodging/Log/Activity/LogInManager.java
@@ -2,14 +2,12 @@ package easylodging.com.ethazi_easylodging.Log.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import easylodging.com.ethazi_easylodging.Log.Fragment.SignIn;
 import easylodging.com.ethazi_easylodging.Main.NavigationDrawer;
 import easylodging.com.ethazi_easylodging.R;
 
@@ -19,12 +17,11 @@ public class LogInManager extends AppCompatActivity {
     private static final String TAG_SIGNIN = "Lanzando SignIn";
 
     protected void onCreate (Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
         setContentView(R.layout.activity_login);
+        super.onCreate(savedInstanceState);
 
         Button btn_LogIn= findViewById(R.id.login_btn);
         TextView txv_Signin= findViewById(R.id.login_signup);
@@ -46,13 +43,16 @@ public class LogInManager extends AppCompatActivity {
 
     private void launchSignIn() {
         Log.d(TAG_SIGNIN, "Lanzando SignIn");
-        Intent intent = new Intent(LogInManager.this, SignInManager.class);
+        Intent intent = new Intent(this, SignInManager.class);
         startActivity(intent);
+        finish();
     }
 
     private void userValidation() {
         Log.d(TAG_LOGIN, "Se ha clicado");
+        //TODO Verificacion de usuario
         Intent intent = new Intent(this, NavigationDrawer.class);
         startActivity(intent);
+        finish();
     }
 }
