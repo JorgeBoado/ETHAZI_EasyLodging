57eab3feeb0192afbafd9b1719956a2b44e375b8
JorgeBoado <jorge.boadogo@elorrieta-errekamari.com>
2019-02-06 09:04:09 +0100

    Implemantacion idioma, no funciona al volver al LogIn

--- app/src/main/java/easylodging/com/ethazi_easylodging/Main/NavigationDrawer.java
+++ app/src/main/java/easylodging/com/ethazi_easylodging/Main/NavigationDrawer.java
@@ -3,6 +3,8 @@ package easylodging.com.ethazi_easylodging.Main;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.design.widget.NavigationView;
@@ -12,17 +14,27 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 import easylodging.com.ethazi_easylodging.Log.Activity.LogInManager;
 import easylodging.com.ethazi_easylodging.Main.Fragment.GridView;
 import easylodging.com.ethazi_easylodging.R;
 
-public class NavigationDrawer extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class NavigationDrawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    public static final String LANG_ES = "es";
+    public static final String LANG_EN = "en-GB";
+    public static final String LANG_DEFAULT = LANG_EN;
 
+    public String LANG_CURRENT;
+    private boolean mUpdated;
+
+    private static final String EXTRA_LANG = "currrent_language";
+    private static final String EXTRA_UPDATED = "language_updated";
 
     public static Intent newIntent(Context context) {
         Intent intent = new Intent();
@@ -54,6 +66,10 @@ public class NavigationDrawer extends AppCompatActivity
             fragmentTransaction.add(R.id.grid_fragment, mGridView, null);
             fragmentTransaction.commit();
         }
+        mUpdated = false;
+        LANG_CURRENT = getIntent().getStringExtra(EXTRA_LANG);
+
+
     }
 
     @Override
@@ -105,7 +121,13 @@ public class NavigationDrawer extends AppCompatActivity
         } else if (id == R.id.nav_map) {
             //TODO Jon, mete aqui tu intent para le mapa.
         } else if (id == R.id.nav_language) {
-            changeLanguage();
+            if (!mUpdated) {
+                if (LANG_CURRENT == null) {
+                    changeLanguage(LANG_DEFAULT, true);
+                } else {
+                    changeLanguage(LANG_CURRENT, true);
+                }
+            }
         } else if (id == R.id.nav_dark_mode) {
 
         } else if (id == R.id.nav_info) {
@@ -129,7 +151,39 @@ public class NavigationDrawer extends AppCompatActivity
                 .show();
     }
 
-    private void changeLanguage() {
-        Toast.makeText(this, getString(R.string.nav_grid_item), Toast.LENGTH_LONG).show();
+    private void changeLanguage(String lang, boolean updated) {
+        Toast.makeText(this, lang, Toast.LENGTH_LONG).show();
+        if(lang.equals(LANG_EN)) {
+            lang = LANG_ES;
+        }else {
+            lang= LANG_EN;
+        }
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.setLocale(myLocale);
+        res.updateConfiguration(conf, dm);
+
+        LANG_CURRENT = lang;
+        mUpdated = updated;
+
+        refresh();
+    }
+    /*public void changeLanguage(String lang) {
+        changeLanguage(lang, false);
+    }*/
+    private void refresh() {
+        Intent intent = new Intent(this, NavigationDrawer.class);
+        intent.putExtra(EXTRA_LANG, LANG_CURRENT);
+        intent.putExtra(EXTRA_UPDATED, true);
+
+        startActivity(intent);
+        finish();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        getIntent().removeExtra(EXTRA_UPDATED);
     }
 }
